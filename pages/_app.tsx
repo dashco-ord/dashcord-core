9365b4d5ed4352c0f955cd3a7f9c4720aa84f3d1
TakshakRamteke <takshakramteke0708@gmail.com>
2022-04-18 04:45:19 +0530

    Feat : schema for users

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// pages/_app.js
+import React from "react";
+import { UserProvider } from "@auth0/nextjs-auth0";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: any) {
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
-
-export default MyApp
