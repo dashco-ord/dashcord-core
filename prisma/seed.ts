0dc5ff483cc30d26af53fbeee1eae2ab6b024257
TakshakRamteke <takshakramteke0708@gmail.com>
2022-04-22 23:44:14 +0530

    Feat : Added seed for database

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,15 +1,17 @@
 import { PrismaClient } from "@prisma/client";
+import { faker } from "@faker-js/faker";
 
 const prisma = new PrismaClient();
 
 const seed = async () => {
-  for (let i = 0; i <= 10; i++) {
+  console.log("Creating Teachers...");
+  for (let i = 0; i <= 100; i++) {
     await prisma.teacher.create({
       data: {
-        name: "Some Name",
-        email: "someemail@gmail.com",
-        phoneNo: "4440026",
-        pictureUrl: "https://somename.com",
+        name: faker.name.findName(),
+        email: faker.internet.email(),
+        phoneNo: faker.phone.phoneNumber(),
+        pictureUrl: faker.internet.url(),
       },
     });
   }
