4073e4aced56c232a84c3c81f50e21527a9bbef5
TakshakRamteke <takshakramteke0708@gmail.com>
2022-04-23 00:19:14 +0530

    feat : DB seed for TGIncharge

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,8 +4,42 @@ import { faker } from "@faker-js/faker";
 const prisma = new PrismaClient();
 
 const seed = async () => {
-  console.log("Creating Teachers...");
-  for (let i = 0; i <= 100; i++) {
+  console.log("🪓 Deleting existing Data..");
+  await prisma.hod.deleteMany();
+  await prisma.tgIncharge.deleteMany();
+  await prisma.teacher.deleteMany();
+
+  console.log("\n🐧 Starting seed...\n");
+
+  console.log("🤵 Creating HOD...");
+  for (let i = 0; i <= 10; i++) {
+    await prisma.hod.create({
+      //@ts-ignore
+      data: {
+        name: faker.name.findName(),
+        email: faker.internet.email(),
+        gender: faker.address.countryCode(),
+        portfolio: faker.internet.url(),
+        pictureUrl: faker.internet.url(),
+        bio: faker.address.secondaryAddress(),
+        department: faker.address.countryCode(),
+      },
+    });
+  }
+
+  console.log("👨 Creating TG Incharge's...");
+  for (let i = 0; i <= 10; i++) {
+    await prisma.tgIncharge.create({
+      data: {
+        name: faker.name.findName(),
+        email: faker.internet.email(),
+        pictureUrl: faker.internet.url(),
+      },
+    });
+  }
+
+  console.log("👨‍🏫 Creating Teachers...");
+  for (let i = 0; i <= 10; i++) {
     await prisma.teacher.create({
       data: {
         name: faker.name.findName(),
