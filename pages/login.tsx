c5dc544918cc352664c766abd49099add91524fa
SwapnilDakhole <swapnildakhole242@gmail.com>
2022-05-03 12:29:38 +0530

    fix : delete unessary file and folder

--- pages/login.tsx
+++ pages/login.tsx
@@ -4,12 +4,12 @@ const Login = () => {
       <form action="">
         <div>
           <label for="username">Username</label>
-          <input type="text" placeholder="Enter Username" name="uname" required>
+          <input type="text" placeholder="Enter Username" name="username" required>
           <ErrorMessage name="username" component="div" />
         </div>
         <div>
           <label for="password">Password</label>
-          <input type="password" placeholder="Enter Password" name="psw" required>
+          <input type="password" placeholder="Enter Password" name="password" required>
           <ErrorMessage name="password" component="div" />
         </div>
         <div>
