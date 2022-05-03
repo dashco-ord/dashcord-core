d391a0a564e3448d24b46cc304f1c0f272344033
TakshakRamteke <takshakramteke0708@gmail.com>
2022-05-03 13:41:37 +0530

    Fix : UI improvement on login form

--- pages/login.tsx
+++ pages/login.tsx
@@ -4,9 +4,9 @@ const LoginPage = () => {
   return (
     <div className='flex flex-col h-screen w-full items-center justify-center'>
       <form className=' border-2 border-slate-200 p-10 rounded-lg lg:w-1/5 sm:w-10/12 xl:w-1/5 '>
-        <div className='flex flex-col pb-5'>
+        <div className='flex flex-col pb-6'>
           <label className='text-xl text-slate-600 font-semibold mr-5 pb-2'>
-            Email :{" "}
+            Email :
           </label>
           <input
             className='p-2 rounded-sm border-b-2 border-b-gray-300 focus:outline-none focus:border-blue-500 transition ease-in-out delay-75 bg-slate-100 duration-75'
@@ -16,10 +16,18 @@ const LoginPage = () => {
           />
         </div>
 
-        <div className='flex flex-col pb-2'>
-          <label className='text-xl text-slate-600 font-semibold mr-5 pb-2'>
-            Password :{" "}
-          </label>
+        <div className='flex flex-col pb-3'>
+          <div className='flex flex-wrap items-baseline'>
+            <label className='text-xl text-slate-600 font-semibold mr-auto pb-2'>
+              Password :
+            </label>
+            <div>
+              <Link href='/password/forgot'>
+                <a className='text-sm text-purple-500'>Forgot password ?</a>
+              </Link>
+            </div>
+          </div>
+
           <input
             className='p-2 rounded-sm border-b-2 border-b-gray-300 focus:outline-none focus:border-blue-500 transition ease-in-out delay-75 bg-slate-100 duration-75'
             type='password'
@@ -27,19 +35,20 @@ const LoginPage = () => {
             required
           />
         </div>
-        <p className='text-slate-600 mt-3'>
-          {"Got no account ? here "}
-          <Link href='/signup'>
-            <a className='text-blue-500 hover:underline'>sign up</a>
-          </Link>
-          {" for one"}
-        </p>
+
         <input
           className='mt-5 p-2 w-24 text-white text-md font-semibold bg-purple-500 rounded-full'
           type='submit'
           value='Sign In'
           required
         />
+        <p className='text-slate-600 mt-5'>
+          {"Got no account ? here "}
+          <Link href='/signup'>
+            <a className='text-blue-500 hover:underline'>sign up</a>
+          </Link>
+          {" for one"}
+        </p>
       </form>
     </div>
   );
