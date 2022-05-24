2e0205557ad910770c71b7333b34903d470af259
TakshakRamteke <takshakramteke0708@gmail.com>
2022-05-25 00:42:53 +0530

    Fix : minor styling fixes

--- components/Layout/TgLayout.tsx
+++ components/Layout/TgLayout.tsx
@@ -1,8 +1,12 @@
 import Sidebar from "components/Sidebar/TgSidebar";
+import Head from "next/head";
 
 const Layout = ({ children }: any) => {
   return (
     <div className='flex bg-slate-200 h-screen'>
+      <Head>
+        <title>Dashcord - Home</title>
+      </Head>
       <Sidebar />
       <main className='m-14 h-100 w-full overflow-y-visible'>{children}</main>
     </div>
