6ce2dbd6b6906a47ced880b87390d24bb64c8e2e
TakshakRamteke <takshakramteke0708@gmail.com>
2022-05-28 23:26:42 +0530

    Feat : Added Authentication for Teacher's

---
+++ pages/api/teacher/auth/[...nextauth].ts
@@ -0,0 +1,70 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { prisma } from "lib/prisma";
+import bcrypt from "bcrypt";
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "dashcord",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "Enter your Email",
+        },
+        password: {
+          label: "Password",
+          type: "Password",
+          placeholder: "Enter your Password",
+        },
+      },
+      //@ts-ignore
+      async authorize(credentials) {
+        const user = await prisma.teacher.findUnique({
+          where: { email: credentials?.email },
+        });
+
+        //@ts-ignore
+        if (bcrypt.compare(credentials?.password, user?.passHash)) {
+          return {
+            id: user?.id,
+            name: user?.name,
+            email: user?.email,
+            role: user?.role,
+          };
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+
+  callbacks: {
+    jwt: ({ token, user }) => {
+      if (user) {
+        token.id = user.id;
+        token.rollNo = user.rollNo;
+        token.role = user.role;
+      }
+      return token;
+    },
+    session: ({ session, token }) => {
+      if (token) {
+        session.id = token.id;
+        session.rollNo = token.rollNo;
+        session.role = token.role;
+      }
+      return session;
+    },
+  },
+
+  secret: process.env.NEXT_AUTH_SECRET,
+  jwt: {
+    secret: process.env.NEXT_AUTH_JWT_SECRET,
+  },
+
+  pages: {
+    signIn: "/incharge/login",
+  },
+});
