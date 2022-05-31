b0fad168172a7c3a1f749424e2a995f3365f9002
SwapnilDakhole <swapnildakhole242@gmail.com>
2022-05-31 17:53:46 +0530

    Feat : redesign for TG/students/[slug] page

--- pages/TG/index.tsx
+++ pages/TG/index.tsx
@@ -43,8 +43,8 @@ const Home: NextPage = () => {
   if (!session) {
     return (
       <Layout>
-        <div className='flex items-center justify-center h-4/5'>
-          <h1 className='text-2xl font-semibold text-white'>
+        <div className="flex items-center justify-center h-4/5">
+          <h1 className="text-2xl font-semibold text-white">
             Please Login first !!!
           </h1>
         </div>
@@ -57,59 +57,65 @@ const Home: NextPage = () => {
         {toast && (
           <Toast
             type={toast.type}
-            className='mb-5'
+            className="mb-5"
             open={true}
-            setOpen={() => setToast(undefined)}>
+            setOpen={() => setToast(undefined)}
+          >
             {toast.message}
           </Toast>
         )}
       </div>
-      <div className=' flex '>
+      <div>
+        <h1 className="mb-5 text-xl font-bold">
+          Welcome, {session.user?.name} 👋
+        </h1>
+      </div>
+      <div className=" flex ">
         <Card
-          title='All Students'
+          title="All Students"
           value={attendances.length}
           link={`/TG/students`}
         />
         <Card
-          title='Weak Students'
+          title="Weak Students"
           value={
             //@ts-ignore
             parseInt(attendances.length / 3)
           }
         />
         <Card
-          title='Avg. Students'
+          title="Avg. Students"
           value={
             //@ts-ignore
             parseInt(attendances.length / 3)
           }
         />
         <Card
-          title='Good Students'
+          title="Good Students"
           value={
             //@ts-ignore
             parseInt(attendances.length / 3)
           }
         />
       </div>
-      <div className='mt-10 flex'>
-        <div className='mr-10'>
-          <Table title='Achievements' headings={["Name", "Achievement"]}>
-            <tr className='divide-black'>
-              <td className='pl-5 p-2 whitespace-nowrap'>Student Name</td>
-              <td className='p-2 whitespace-nowrap'>won a hackathon</td>
+      {/* <div className="mt-10 flex">
+        <div className="mr-10">
+          <Table title="Achievements" headings={["Name", "Achievement"]}>
+            <tr className="divide-black">
+              <td className="pl-5 p-2 whitespace-nowrap">Student Name</td>
+              <td className="p-2 whitespace-nowrap">won a hackathon</td>
             </tr>
-            <tr className='divide-black'>
-              <td className='pl-5 p-2 whitespace-nowrap'>Student Name</td>
-              <td className='p-2 whitespace-nowrap'>Won a hackathon</td>
+            <tr className="divide-black">
+              <td className="pl-5 p-2 whitespace-nowrap">Student Name</td>
+              <td className="p-2 whitespace-nowrap">Won a hackathon</td>
             </tr>
           </Table>
         </div>
-      </div>
-      <div className='pt-11'>
+      </div> */}
+      <div className="pt-11">
         <Table
           refresh={getAttendance}
-          title='Attendance per Lecture'
+          title="Attendance per Lecture"
           headings={[
             "name",
             "rollNo",
@@ -119,74 +125,81 @@ const Home: NextPage = () => {
             "lecture4",
             "lecture5",
             "lecture6",
-          ]}>
+          ]}
+        >
           {attendances.map((attendance) =>
             attendance.Attendance ? (
               <tr key={attendance.Attendance.id}>
-                <td className='pl-5 p-2 whitespace-nowrap text-violet-500'>
+                <td className="pl-5 p-2 whitespace-nowrap text-violet-500">
                   {attendance.Attendance && (
                     <Link href={`/TG/students/${attendance.id}`}>
                       <a>{attendance.name}</a>
                     </Link>
                   )}
                 </td>
-                <td className='p-2 whitespace-nowrap'>{attendance.rollNo}</td>
-                <td className='p-2 whitespace-nowrap'>
-                  <div className='flex items-center'>
+                <td className="p-2 whitespace-nowrap">{attendance.rollNo}</td>
+                <td className="p-2 whitespace-nowrap">
+                  <div className="flex items-center">
                     <div
                       className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
                         attendance.Attendance.lecture1
-                      )}`}>
+                      )}`}
+                    >
                       {attendance.Attendance.lecture1}
                     </div>
                   </div>
                 </td>
-                <td className='p-2 whitespace-nowrap'>
-                  <div className='flex items-center'>
+                <td className="p-2 whitespace-nowrap">
+                  <div className="flex items-center">
                     <div
                       className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
                         attendance.Attendance.lecture2
-                      )}`}>
+                      )}`}
+                    >
                       {attendance.Attendance.lecture2}
                     </div>
                   </div>
                 </td>
-                <td className='p-2 whitespace-nowrap'>
-                  <div className='flex items-center'>
+                <td className="p-2 whitespace-nowrap">
+                  <div className="flex items-center">
                     <div
                       className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
                         attendance.Attendance.lecture3
-                      )}`}>
+                      )}`}
+                    >
                       {attendance.Attendance.lecture3}
                     </div>
                   </div>
                 </td>
-                <td className='p-2 whitespace-nowrap'>
-                  <div className='flex items-center'>
+                <td className="p-2 whitespace-nowrap">
+                  <div className="flex items-center">
                     <div
                       className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
                         attendance.Attendance.lecture4
-                      )}`}>
+                      )}`}
+                    >
                       {attendance.Attendance.lecture4}
                     </div>
                   </div>
                 </td>
-                <td className='p-2 whitespace-nowrap'>
-                  <div className='flex items-center'>
+                <td className="p-2 whitespace-nowrap">
+                  <div className="flex items-center">
                     <div
                       className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
                         attendance.Attendance.lecture5
-                      )}`}>
+                      )}`}
+                    >
                       {attendance.Attendance.lecture5}
                     </div>
                   </div>
                 </td>
-                <td className='p-2 whitespace-nowrap'>
-                  <div className='flex items-center'>
+                <td className="p-2 whitespace-nowrap">
+                  <div className="flex items-center">
                     <div
                       className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
                         attendance.Attendance.lecture6
-                      )}`}>
+                      )}`}
+                    >
                       {attendance.Attendance.lecture6}
                     </div>
                   </div>
@@ -195,12 +208,12 @@ const Home: NextPage = () => {
             ) : (
               //@ts-ignore
               <tr key={attendance.name}>
-                <td className='pl-5 p-2 whitespace-nowrap text-violet-500'>
+                <td className="pl-5 p-2 whitespace-nowrap text-violet-500">
                   <Link href={`/tg/students/${attendance.id}`}>
                     <a>{attendance.name}</a>
                   </Link>
                 </td>
-                <td className='p-2 whitespace-nowrap'>{attendance.rollNo}</td>
+                <td className="p-2 whitespace-nowrap">{attendance.rollNo}</td>
               </tr>
             )
           )}
