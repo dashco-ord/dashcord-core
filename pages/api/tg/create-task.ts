75a92e977f5b08ff15c008c2b01d6ed3359ad6d2
SwapnilDakhole <swapnildakhole242@gmail.com>
2022-06-02 13:23:29 +0530

    Feat : feature for TG to add tasks for students

--- pages/api/tg/create-task.ts
+++ pages/api/tg/create-task.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
 import { prisma } from "lib/prisma";
+import { Status } from "@prisma/client";
 
 const createTaskRoute = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method == "POST") {
@@ -14,12 +15,14 @@ const createTaskRoute = async (req: NextApiRequest, res: NextApiResponse) => {
           description: description,
           deadlineDate: deadlineDate,
           deadlineTime: deadlineTime,
+          taskStatus: Status.InProgress,
           //@ts-ignore
           tgId: tgId,
         },
       });
       res.status(200).end();
     } catch (error) {
+      console.log(error);
       res.status(500).end();
     }
   }
