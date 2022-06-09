fe9a7ec1fb0a4a93af375f81b9d0d48884d4d43c
TakshakRamteke <takshakramteke0708@gmail.com>
2022-06-10 01:10:02 +0530

    Feat : upload certificate

--- pages/TG/students/[slug].tsx
+++ pages/TG/students/[slug].tsx
@@ -222,6 +222,23 @@ const SingleStudentPage = ({
     }
   };
 
+  const [cert, setCert] = useState("");
+
+  const handleCert = async (e: any) => {
+    e.preventDefault();
+    const data = new FormData();
+    data.append("file", cert);
+    data.append("upload_preset", "certificates");
+    const res = await fetch(
+      "https://api.cloudinary.com/v1_1/dashcord/image/upload",
+      {
+        method: "POST",
+        body: data,
+      }
+    ).then((res) => res.json());
+    console.log(res);
+  };
+
   return (
     <Layout>
       <main>
@@ -374,6 +391,21 @@ const SingleStudentPage = ({
                     />
                   </div>
                 </div>
+              </form>
+            </div>
+            <div>
+              <h1>Certificates : </h1>
+              <form onSubmit={handleCert}>
+                <input
+                  type='file'
+                  onChange={(e) =>
+                    setCert(
+                      //@ts-ignore
+                      e.target.files[0]
+                    )
+                  }
+                />
+                <input type='submit' value='Upload.' />
               </form>
             </div>
           </div>
