5747d990c29dc8b624ab666c48633f7664df1264
SwapnilDakhole <swapnildakhole242@gmail.com>
2022-08-13 21:34:41 +0530

    fix : fixed some version 1 feature

---
+++ lib/xlSchema.ts
@@ -0,0 +1,53 @@
+export const AssesmentSchema = {
+  "ROLL NO": {
+    prop: "studentId",
+  },
+  "STUDENT NAME": {
+    prop: "name",
+  },
+  "SUBJECT 1": {
+    prop: "score1",
+  },
+  "SUBJECT 2": {
+    prop: "score2",
+  },
+  "SUBJECT 3": {
+    prop: "score3",
+  },
+  "SUBJECT 4": {
+    prop: "score4",
+  },
+  "SUBJECT 5": {
+    prop: "score5",
+  },
+  "SUBJECT 6": {
+    prop: "score6",
+  },
+};
+
+export const AttendanceSchema = {
+  "ROLL NO": {
+    prop: "studentId",
+  },
+  "STUDENT NAME": {
+    prop: "name",
+  },
+  "LECTURE 1": {
+    prop: "lecture1",
+  },
+  "LECTURE 2": {
+    prop: "lecture2",
+  },
+  "LECTURE 3": {
+    prop: "lecture3",
+  },
+  "LECTURE 4": {
+    prop: "lecture4",
+  },
+  "LECTURE 5": {
+    prop: "lecture5",
+  },
+  "LECTURE 6": {
+    prop: "lecture6",
+  },
+};
