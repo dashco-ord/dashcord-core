dccb67f041ba7ad130aa694622f42eafe3ce5091
TakshakRamteke <takshakramteke0708@gmail.com>
2022-08-29 22:58:44 +0530

    FEAT : User page for TG's

---
+++ pages/tg/user.tsx
@@ -0,0 +1,133 @@
+import TgsLayout from "components/Layouts/TgsLayout";
+import { prisma } from "lib/prisma";
+import { Tg, UserRole } from "@prisma/client";
+import { checkUserRoleAndRedirect } from "lib/checks";
+import { getSession } from "next-auth/react";
+
+export async function getServerSideProps(context: any) {
+  const session = await getSession(context);
+  const tg = await prisma.tg.findUnique({
+    where: {
+      //@ts-ignore
+      id: session?.id,
+    },
+  });
+  return checkUserRoleAndRedirect(context, UserRole.TG, {
+    extra: { tg: JSON.parse(JSON.stringify(tg)) },
+  });
+}
+
+type userPageProps = {
+  tg: Tg;
+};
+
+const User = ({ tg }: userPageProps) => {
+  return (
+    <TgsLayout>
+      <main>
+        <div className='flex flex-wrap bg-white rounded-lg p-8'>
+          <form className='flex flex-row-reverse'>
+            <div>
+              <div className='flex w-80 h-80 bg-slate-200  rounded-md ml-60'>
+                <img
+                  //@ts-ignore
+                  src={tg?.pictureUrl}
+                  width={350}
+                  height={350}
+                />
+              </div>
+            </div>
+            <div>
+              <div className='flex'>
+                <div className='flex flex-col pb-6 mr-8'>
+                  <label className='text-2xl font-semibold mr-5 pb-2'>
+                    Name :
+                  </label>
+                  <input
+                    className='p-2 pl-0 rounded-sm bg-white text-xl border-b-2 border-b-gray-500 focus:outline-none focus:border-blue-500 transition ease-in-out delay-75 duration-75'
+                    type='text'
+                    placeholder='Enter your Name'
+                    defaultValue={tg?.name}
+                    required
+                  />
+                </div>
+                <div className='flex flex-col pb-6 mr-8'>
+                  <label className='text-2xl font-semibold mr-5 pb-2'>
+                    Phone.no :
+                  </label>
+                  <input
+                    className='p-2 pl-0 rounded-sm bg-white text-xl border-b-2 border-b-gray-500 focus:outline-none focus:border-blue-500 transition ease-in-out delay-75 duration-75'
+                    type='text'
+                    placeholder='Enter your Roll.No'
+                    //@ts-ignore
+                    defaultValue={tg?.phoneNo}
+                    required
+                  />
+                </div>
+                <div className='flex flex-col pb-6 mr-8'>
+                  <label className='text-2xl font-semibold mr-5 pb-2'>
+                    Email :
+                  </label>
+                  <input
+                    className='w-96 p-2 pl-0 rounded-sm bg-white text-xl border-b-2 border-b-gray-500 focus:outline-none focus:border-blue-500 transition ease-in-out delay-75 duration-75'
+                    type='email'
+                    placeholder='Enter your Email'
+                    defaultValue={tg?.email}
+                    required
+                  />
+                </div>
+              </div>
+
+              <div className='flex'>
+                <div className='flex flex-col pb-6 mr-8'>
+                  <label className='text-2xl font-semibold mr-5 pb-2'>
+                    Department :
+                  </label>
+                  <input
+                    className='p-2 pl-0 rounded-sm bg-white text-xl border-b-2 border-b-gray-500 focus:outline-none focus:border-blue-500 transition ease-in-out delay-75 duration-75'
+                    type='text'
+                    placeholder='Enter your Department'
+                    //@ts-ignore
+                    defaultValue={tg?.department}
+                    required
+                  />
+                </div>
+
+                <div className='flex flex-col pb-6 mr-8'>
+                  <label className='text-2xl font-semibold mr-5 pb-2'>
+                    Gender :
+                  </label>
+                  <input
+                    className='p-2 pl-0 rounded-sm bg-white text-xl border-b-2 border-b-gray-500 focus:outline-none focus:border-blue-500 transition ease-in-out delay-75 duration-75'
+                    type='text'
+                    placeholder='Enter your Gender'
+                    //@ts-ignore
+                    defaultValue={tg?.gender}
+                    required
+                  />
+                </div>
+              </div>
+              <div className='flex'>
+                <div className='flex flex-col pb-6 mr-8'>
+                  <label className='text-2xl font-semibold mr-5 pb-2 w-fit'>
+                    Bio :
+                  </label>
+                  <input
+                    className='p-2 pl-0 rounded-sm bg-white text-xl border-b-2 border-b-gray-500 focus:outline-none focus:border-blue-500 transition ease-in-out delay-75 duration-75'
+                    type='text'
+                    placeholder='Enter your Bio'
+                    //@ts-ignore
+                    defaultValue={tg?.bio}
+                    required
+                  />
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </main>
+    </TgsLayout>
+  );
+};
+
+export default User;
