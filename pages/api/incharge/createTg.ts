4ab4cf752e06155e3fcb62feb928bd939d1abaef
TakshakRamteke <takshakramteke0708@gmail.com>
2022-08-29 23:32:20 +0530

    Feat : TG incharge Workflow

---
+++ pages/api/incharge/createTg.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient, UserRole } from "@prisma/client";
+import { getSession } from "next-auth/react";
+import bcrypt from "bcrypt";
+
+const createTg = async (req: NextApiRequest, res: NextApiResponse) => {
+  const prisma = new PrismaClient();
+  const session = await getSession({ req });
+  if (req.method == "POST") {
+    const { name, phoneNo, email, department, gender } = await req.body;
+    try {
+      if (session?.role == UserRole.INCHARGE) {
+        const tg = await prisma.tg.create({
+          data: {
+            name: name,
+            phoneNo: phoneNo,
+            email: email,
+            department: department,
+            gender: gender,
+            passHash: await bcrypt.hash(email, 10),
+          },
+        });
+        res.send(tg);
+        res.status(200).end();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  res.status(405).end();
+};
+
+export default createTg;
