1c33d9e7858b719ef7181daeb4557f3ccdec2d9a
TakshakRamteke <takshakramteke0708@gmail.com>
2022-09-03 23:06:32 +0530

    FIX : API route for updating user details.

---
+++ pages/api/updateDetails.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+const updateDetails = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method == "POST") {
+    //update User Details
+  }
+};
+
+export default updateDetails;
