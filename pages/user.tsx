b53acec47dc36c9cf981b1eba9d120504f08194b
SwapnilDakhole <swapnildakhole242@gmail.com>
2022-09-17 13:24:03 +0530

    FIX : Auto salutations + details form

--- pages/user.tsx
+++ pages/user.tsx
@@ -1,13 +1,33 @@
-import { UserRole } from "@prisma/client";
+import { Student, UserRole } from "@prisma/client";
 import StudentsLayout from "components/Layouts/StudentsLayout";
 import { checkUserRoleAndRedirect } from "lib/checks";
+import { getSession } from "next-auth/react";
+import Details from "./details";
+import { prisma } from "lib/prisma";
+import PersonalDetailForm from "components/DataForms/PersonalDetail";
+
+type UserProps = {
+  student: Student;
+};
 
 export const getServerSideProps = async (context: any) => {
-  return checkUserRoleAndRedirect(context, UserRole.STUDENT, {});
+  const session = await getSession(context);
+  const student = await prisma.student.findUnique({
+    where: {
+      //@ts-ignore
+      rollNo: session?.id,
+    },
+  });
+  return checkUserRoleAndRedirect(context, UserRole.STUDENT, {
+    extra: { student: JSON.parse(JSON.stringify(student)) },
+  });
 };
 
-const UserPage = () => {
-  return <StudentsLayout></StudentsLayout>;
-};
-
-export default UserPage;
+export default function UserPage({ student }: UserProps) {
+  return (
+    <StudentsLayout>
+      {!student && <Details />}
+      {student && <PersonalDetailForm student={student} />}
+    </StudentsLayout>
+  );
+}
