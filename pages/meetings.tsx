e803b31ec4580c98480973b8cf4afdad15f67d15
SwapnilDakhole <swapnildakhole242@gmail.com>
2022-09-26 12:01:15 +0530

    FIX : Some front end part of meetings

--- pages/meetings.tsx
+++ pages/meetings.tsx
@@ -30,29 +30,23 @@ export default function MeetingsPage({ meetings }: meetingsprops) {
   return (
     <StudentsLayout>
       {meetings.map((meeting) => (
-        <div>
-          {meetings.map((meeting) => (
-            <div key={meeting.id}>
-              <div className="flex flex-col break-words min-h-[13rem] w-[25rem] font-semibold mt-10 mr-10 border border-black p-5 rounded">
-                <div className="flex items-center">
-                  <div className="text-3xl mb-2 font-bold p-1">
-                    {meeting.title}
-                  </div>
-                </div>
-                <div className=" text-slate-700">{`Recorded At : ${moment(
-                  meeting.createdAt
-                ).format("MMMM Do YYYY, h:mm:ss a")}`}</div>
-                <div className=" text-slate-700">{`Created By : ${
-                  //@ts-ignore
-                  meeting?.TG.name
-                }`}</div>
-
-                <div className="mt-2 text-justify text-lg">
-                  {meeting.description}
-                </div>
-              </div>
+        <div key={meeting.id}>
+          <div className="flex flex-col break-words min-h-[13rem] w-[25rem] font-semibold mt-10 mr-10 border border-black p-5 rounded">
+            <div className="flex items-center">
+              <div className="text-3xl mb-2 font-bold p-1">{meeting.title}</div>
             </div>
-          ))}
+            <div className=" text-slate-700">{`Recorded At : ${moment(
+              meeting.createdAt
+            ).format("MMMM Do YYYY, h:mm:ss a")}`}</div>
+            <div className=" text-slate-700">{`Created By : ${
+              //@ts-ignore
+              meeting?.TG.name
+            }`}</div>
+
+            <div className="mt-2 text-justify text-lg">
+              {meeting.description}
+            </div>
+          </div>
         </div>
       ))}
     </StudentsLayout>
