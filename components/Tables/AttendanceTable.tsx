1a33d769c2ed76cc7baccd79e98804271d7cd410
TakshakRamteke <takshakramteke0708@gmail.com>
2022-09-26 14:35:00 +0530

    FIX : Attendances not showing

--- components/Tables/AttendanceTable.tsx
+++ components/Tables/AttendanceTable.tsx
@@ -1,13 +1,13 @@
-import { Attendance } from "@prisma/client";
+import { Attendances } from "@prisma/client";
 import Table from "./Table";
 import attendanceColors from "components/AttendanceColor";
 import moment from "moment";
 
-const AttendanceTable = ({ attendance }: AttendanceTableProps) => {
+const AttendanceTable = ({ attendances }: AttendanceTableProps) => {
   return (
-    <div className="ml-10 mt-2 mb-2 md:w-[50rem] md:ml-0 md:mt-4">
+    <div className='ml-10 mt-2 mb-2 md:w-[50rem] md:ml-0 md:mt-4'>
       <Table
-        title="Attendance Today : "
+        title='Attendance Today : '
         headings={[
           "date",
           "lecture 1",
@@ -17,119 +17,80 @@ const AttendanceTable = ({ attendance }: AttendanceTableProps) => {
           "lecture 5",
           "lecture 6",
         ]}
-        noShadow={true}
-      >
-        {
-          //@ts-ignore
-          attendance ? (
-            <tr className="font-semibold">
-              <td className="pl-5 p-2 whitespace-nowrap">
-                <div>
-                  {
-                    //@ts-ignore
-                    moment(attendance.updatedAt).format("MMM Do YYYY")
-                  }
+        noShadow={true}>
+        {attendances?.slice(-1).map((attendance) => (
+          <tr className='font-semibold' key={attendance.id}>
+            <td className='pl-5 p-2 whitespace-nowrap'>
+              <div>{moment(attendance.date).format("MMM Do YYYY")}</div>
+            </td>
+
+            <td className='p-2 whitespace-nowrap'>
+              <div className='flex items-center'>
+                <div
+                  className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
+                    attendance.lecture1
+                  )}`}>
+                  {attendance.lecture1}
                 </div>
-              </td>
-              <td className="p-2 whitespace-nowrap">
-                <div className="flex items-center">
-                  <div
-                    className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
-                      //@ts-ignore
-                      attendance.lecture1
-                    )}`}
-                  >
-                    {
-                      //@ts-ignore
-                      attendance.lecture1
-                    }
-                  </div>
+              </div>
+            </td>
+
+            <td className='p-2 whitespace-nowrap'>
+              <div className='flex items-center'>
+                <div
+                  className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
+                    attendance.lecture2
+                  )}`}>
+                  {attendance.lecture2}
                 </div>
-              </td>
-              <td className="p-2 whitespace-nowrap">
-                <div className="flex items-center">
-                  <div
-                    className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
-                      //@ts-ignore
-                      attendance.lecture2
-                    )}`}
-                  >
-                    {
-                      //@ts-ignore
-                      attendance.lecture2
-                    }
-                  </div>
+              </div>
+            </td>
+
+            <td className='p-2 whitespace-nowrap'>
+              <div className='flex items-center'>
+                <div
+                  className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
+                    attendance.lecture3
+                  )}`}>
+                  {attendance.lecture3}
                 </div>
-              </td>
-              <td className="p-2 whitespace-nowrap">
-                <div className="flex items-center">
-                  <div
-                    className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
-                      //@ts-ignore
-                      attendance.lecture3
-                    )}`}
-                  >
-                    {
-                      //@ts-ignore
-                      attendance.lecture3
-                    }
-                  </div>
+              </div>
+            </td>
+
+            <td className='p-2 whitespace-nowrap'>
+              <div className='flex items-center'>
+                <div
+                  className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
+                    attendance.lecture4
+                  )}`}>
+                  {attendance.lecture4}
                 </div>
-              </td>
-              <td className="p-2 whitespace-nowrap">
-                <div className="flex items-center">
-                  <div
-                    className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
-                      //@ts-ignore
-                      attendance.lecture4
-                    )}`}
-                  >
-                    {
-                      //@ts-ignore
-                      attendance.lecture4
-                    }
-                  </div>
+              </div>
+            </td>
+
+            <td className='p-2 whitespace-nowrap'>
+              <div className='flex items-center'>
+                <div
+                  className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
+                    attendance.lecture5
+                  )}`}>
+                  {attendance.lecture5}
                 </div>
-              </td>
-              <td className="p-2 whitespace-nowrap">
-                <div className="flex items-center">
-                  <div
-                    className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
-                      //@ts-ignore
-                      attendance.lecture5
-                    )}`}
-                  >
-                    {
-                      //@ts-ignore
-                      attendance.lecture5
-                    }
-                  </div>
+              </div>
+            </td>
+
+            <td className='p-2 whitespace-nowrap'>
+              <div className='flex items-center'>
+                <div
+                  className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
+                    attendance.lecture6
+                  )}`}>
+                  {attendance.lecture6}
                 </div>
-              </td>
-              <td className="p-2 whitespace-nowrap">
-                <div className="flex items-center">
-                  <div
-                    className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
-                      //@ts-ignore
-                      attendance.lecture6
-                    )}`}
-                  >
-                    {
-                      //@ts-ignore
-                      attendance.lecture6
-                    }
-                  </div>
-                </div>
-              </td>
-            </tr>
-          ) : (
-            <tr>
-              <td className="pl-5 p-2 whitespace-nowrap font-semibold">
-                Attendance is yet to be Marked
-              </td>
-            </tr>
-          )
-        }
+              </div>
+            </td>
+          </tr>
+        ))}
       </Table>
     </div>
   );
@@ -138,5 +99,5 @@ const AttendanceTable = ({ attendance }: AttendanceTableProps) => {
 export default AttendanceTable;
 
 type AttendanceTableProps = {
-  attendance?: Attendance | null;
+  attendances: Attendances[];
 };
