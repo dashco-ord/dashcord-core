0737c428b6dba3fa0ecf707061da4a8d4704dc7e
TakshakRamteke <takshakramteke0708@gmail.com>
2022-09-29 21:02:31 +0530

    FEAT : Attendance History table

--- components/Tables/AttendanceHistory.tsx
+++ components/Tables/AttendanceHistory.tsx
@@ -1,4 +1,7 @@
 import { Attendances } from "@prisma/client";
+import moment from "moment";
+import Table from "./Table";
+import attendanceColors from "components/AttendanceColor";
 
 type AttendanceHistoryProps = {
   attendances: Attendances[];
@@ -7,5 +10,95 @@ type AttendanceHistoryProps = {
 export default function AttendanceHistory({
   attendances,
 }: AttendanceHistoryProps) {
-  return <div>{JSON.stringify(attendances)}</div>;
+  return (
+    <div className='ml-10 mt-16 mb-2 md:w-[50rem] md:ml-0 md:mt-4'>
+      <h2 className='font-bold text-2xl mt-7 mr-auto'>Past Attendances : </h2>
+      <Table
+        title=''
+        headings={[
+          "date",
+          "lecture 1",
+          "lecture 2",
+          "lecture 3",
+          "lecture 4",
+          "lecture 5",
+          "lecture 6",
+        ]}
+        noShadow={true}
+        scroll={true}>
+        {attendances.map((attendance) => (
+          <tr className='font-semibold' key={attendance.id}>
+            <td className='pl-5 p-2 whitespace-nowrap'>
+              <div>{moment(attendance.date).format("MMM Do YYYY")}</div>
+            </td>
+            <td className='p-2 whitespace-nowrap'>
+              <div className='flex items-center'>
+                <div
+                  className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
+                    attendance.lecture1
+                  )}`}>
+                  {attendance.lecture1}
+                </div>
+              </div>
+            </td>
+
+            <td className='p-2 whitespace-nowrap'>
+              <div className='flex items-center'>
+                <div
+                  className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
+                    attendance.lecture2
+                  )}`}>
+                  {attendance.lecture2}
+                </div>
+              </div>
+            </td>
+
+            <td className='p-2 whitespace-nowrap'>
+              <div className='flex items-center'>
+                <div
+                  className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
+                    attendance.lecture3
+                  )}`}>
+                  {attendance.lecture3}
+                </div>
+              </div>
+            </td>
+
+            <td className='p-2 whitespace-nowrap'>
+              <div className='flex items-center'>
+                <div
+                  className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
+                    attendance.lecture4
+                  )}`}>
+                  {attendance.lecture4}
+                </div>
+              </div>
+            </td>
+
+            <td className='p-2 whitespace-nowrap'>
+              <div className='flex items-center'>
+                <div
+                  className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
+                    attendance.lecture5
+                  )}`}>
+                  {attendance.lecture5}
+                </div>
+              </div>
+            </td>
+
+            <td className='p-2 whitespace-nowrap'>
+              <div className='flex items-center'>
+                <div
+                  className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
+                    attendance.lecture6
+                  )}`}>
+                  {attendance.lecture6}
+                </div>
+              </div>
+            </td>
+          </tr>
+        ))}
+      </Table>
+    </div>
+  );
 }
--- components/Tables/Table.tsx
+++ components/Tables/Table.tsx
@@ -7,6 +7,7 @@ type TableProps = {
   children: JSX.Element | JSX.Element[];
   refresh?: MouseEventHandler;
   noShadow?: boolean;
+  scroll?: boolean;
 };
 
 const Table = ({
@@ -15,36 +16,38 @@ const Table = ({
   children,
   refresh,
   noShadow,
+  scroll,
 }: TableProps) => {
   return (
     <div
       className={`col-span-full bg-white shadow-md rounded-md min-w-1/3 ${
         noShadow ? "shadow-none" : ""
-      }`}
-    >
+      } ${scroll ? "overflow-scroll h-52" : ""}`}>
       <header
-        className={`py-4 flex items-center ${noShadow ? "px-0" : "px-5 "}`}
-      >
-        <h2 className="font-bold text-2xl mr-auto">{title}</h2>
+        className={`py-4 flex items-center ${noShadow ? "px-0" : "px-5 "}`}>
+        <h2 className='font-bold text-2xl mr-auto'>{title}</h2>
         {refresh && (
           <FcRefresh
-            className="text-2xl font-bold hover:scale-110"
+            className='text-2xl font-bold hover:scale-110'
             onClick={refresh}
           />
         )}
       </header>
-      <div className="overflow-x-auto">
-        <table className="table-auto w-full divide-y">
-          <thead className="text-xs uppercase text-gray-500 bg-white border-t-2 border-black mt-0">
+      <div className='overflow-x-auto'>
+        <table className='table-auto w-full divide-y'>
+          <thead
+            className={`text-xs uppercase text-gray-500 bg-white border-t-2 border-black mt-0 ${
+              scroll ? "" : ""
+            }`}>
             <tr>
               {headings.map((heading) => (
-                <th key={heading} className="px-2 first:pl-5 last:pr-5 py-3">
-                  <div className="font-semibold text-left">{heading}</div>
+                <th key={heading} className='px-2 first:pl-5 last:pr-5 py-3'>
+                  <div className='font-semibold text-left'>{heading}</div>
                 </th>
               ))}
             </tr>
           </thead>
-          <tbody className="text-md divide-y">{children}</tbody>
+          <tbody className='text-md divide-y'>{children}</tbody>
         </table>
       </div>
     </div>
