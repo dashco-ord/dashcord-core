9bfa136969a684f949d03046bdacce18b5d9f1da
TakshakRamteke <takshakramteke0708@gmail.com>
2022-10-03 20:42:45 +0530

    FIX : <img/> to next/image

--- pages/hod/user.tsx
+++ pages/hod/user.tsx
@@ -3,6 +3,7 @@ import { prisma } from "lib/prisma";
 import { UserRole } from "@prisma/client";
 import { checkUserRoleAndRedirect } from "lib/checks";
 import { getSession } from "next-auth/react";
+import Image from "next/image";
 
 export const getServerSideProps = async (context: any) => {
   const session = await getSession(context);
@@ -19,97 +20,98 @@ const User = ({ user }: any) => {
   return (
     <HodsLayout>
       <main>
-        <div className="flex flex-wrap flex-col bg-white rounded-lg p-8">
-          <form className="flex flex-col lg:flex-row-reverse">
+        <div className='flex flex-wrap flex-col bg-white rounded-lg p-8'>
+          <form className='flex flex-col lg:flex-row-reverse'>
             <div>
-              <div className="flex w-40 h-40 lg:w-80 lg:h-80 bg-slate-200  rounded-md ml-0 lg:ml-60">
-                <img
+              <div className='flex w-40 h-40 lg:w-80 lg:h-80 bg-slate-200  rounded-md ml-0 lg:ml-60'>
+                <Image
                   //@ts-ignore
                   src={user?.pictureUrl}
                   width={350}
                   height={350}
+                  alt={`${user.name}`}
                 />
               </div>
             </div>
             <div>
-              <div className="flex flex-col lg:flex-row">
-                <div className="flex flex-col pb-6 mr-8">
-                  <label className="text-2xl font-semibold mr-5 pb-2">
+              <div className='flex flex-col lg:flex-row'>
+                <div className='flex flex-col pb-6 mr-8'>
+                  <label className='text-2xl font-semibold mr-5 pb-2'>
                     Name :
                   </label>
                   <input
-                    className="p-2 pl-0 rounded-sm bg-white text-xl border-b-2 border-b-gray-500 focus:outline-none focus:border-blue-500 transition ease-in-out delay-75 duration-75"
-                    type="text"
-                    placeholder="Enter your Name"
+                    className='p-2 pl-0 rounded-sm bg-white text-xl border-b-2 border-b-gray-500 focus:outline-none focus:border-blue-500 transition ease-in-out delay-75 duration-75'
+                    type='text'
+                    placeholder='Enter your Name'
                     defaultValue={user?.name}
                     required
                   />
                 </div>
-                <div className="flex flex-col pb-6 mr-8">
-                  <label className="text-2xl font-semibold mr-5 pb-2">
+                <div className='flex flex-col pb-6 mr-8'>
+                  <label className='text-2xl font-semibold mr-5 pb-2'>
                     Phone.no :
                   </label>
                   <input
-                    className="p-2 pl-0 rounded-sm bg-white text-xl border-b-2 border-b-gray-500 focus:outline-none focus:border-blue-500 transition ease-in-out delay-75 duration-75"
-                    type="text"
-                    placeholder="Enter your Phone.No"
+                    className='p-2 pl-0 rounded-sm bg-white text-xl border-b-2 border-b-gray-500 focus:outline-none focus:border-blue-500 transition ease-in-out delay-75 duration-75'
+                    type='text'
+                    placeholder='Enter your Phone.No'
                     //@ts-ignore
                     defaultValue={user?.phoneNo}
                     required
                   />
                 </div>
-                <div className="flex flex-col pb-6 mr-8">
-                  <label className="text-2xl font-semibold mr-5 pb-2">
+                <div className='flex flex-col pb-6 mr-8'>
+                  <label className='text-2xl font-semibold mr-5 pb-2'>
                     Email :
                   </label>
                   <input
-                    className="p-2 pl-0 rounded-sm bg-white text-xl border-b-2 border-b-gray-500 focus:outline-none focus:border-blue-500 transition ease-in-out delay-75 duration-75"
-                    type="email"
-                    placeholder="Enter your Email"
+                    className='p-2 pl-0 rounded-sm bg-white text-xl border-b-2 border-b-gray-500 focus:outline-none focus:border-blue-500 transition ease-in-out delay-75 duration-75'
+                    type='email'
+                    placeholder='Enter your Email'
                     defaultValue={user?.email}
                     required
                   />
                 </div>
               </div>
 
-              <div className="flex flex-col lg:flex-row">
-                <div className="flex flex-col pb-6 mr-8">
-                  <label className="text-2xl font-semibold mr-5 pb-2">
+              <div className='flex flex-col lg:flex-row'>
+                <div className='flex flex-col pb-6 mr-8'>
+                  <label className='text-2xl font-semibold mr-5 pb-2'>
                     Department :
                   </label>
                   <input
-                    className="p-2 pl-0 rounded-sm bg-white text-xl border-b-2 border-b-gray-500 focus:outline-none focus:border-blue-500 transition ease-in-out delay-75 duration-75"
-                    type="text"
-                    placeholder="Enter your Department"
+                    className='p-2 pl-0 rounded-sm bg-white text-xl border-b-2 border-b-gray-500 focus:outline-none focus:border-blue-500 transition ease-in-out delay-75 duration-75'
+                    type='text'
+                    placeholder='Enter your Department'
                     //@ts-ignore
                     defaultValue={user?.department}
                     required
                   />
                 </div>
 
-                <div className="flex flex-col pb-6 mr-8">
-                  <label className="text-2xl font-semibold mr-5 pb-2">
+                <div className='flex flex-col pb-6 mr-8'>
+                  <label className='text-2xl font-semibold mr-5 pb-2'>
                     Gender :
                   </label>
                   <input
-                    className="p-2 pl-0 rounded-sm bg-white text-xl border-b-2 border-b-gray-500 focus:outline-none focus:border-blue-500 transition ease-in-out delay-75 duration-75"
-                    type="text"
-                    placeholder="Enter your Gender"
+                    className='p-2 pl-0 rounded-sm bg-white text-xl border-b-2 border-b-gray-500 focus:outline-none focus:border-blue-500 transition ease-in-out delay-75 duration-75'
+                    type='text'
+                    placeholder='Enter your Gender'
                     //@ts-ignore
                     defaultValue={user?.gender}
                     required
                   />
                 </div>
               </div>
-              <div className="flex flex-col lg:flex-row">
-                <div className="flex flex-col pb-6 mr-8">
-                  <label className="text-2xl font-semibold mr-5 pb-2 w-fit">
+              <div className='flex flex-col lg:flex-row'>
+                <div className='flex flex-col pb-6 mr-8'>
+                  <label className='text-2xl font-semibold mr-5 pb-2 w-fit'>
                     Bio :
                   </label>
                   <input
-                    className="p-2 pl-0 rounded-sm bg-white text-xl border-b-2 border-b-gray-500 focus:outline-none focus:border-blue-500 transition ease-in-out delay-75 duration-75"
-                    type="text"
-                    placeholder="Enter your Bio"
+                    className='p-2 pl-0 rounded-sm bg-white text-xl border-b-2 border-b-gray-500 focus:outline-none focus:border-blue-500 transition ease-in-out delay-75 duration-75'
+                    type='text'
+                    placeholder='Enter your Bio'
                     //@ts-ignore
                     defaultValue={user?.bio}
                     required
