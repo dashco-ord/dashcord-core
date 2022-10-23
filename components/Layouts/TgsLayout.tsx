32d7d4882f030a714a790637f5dd0517fe58dd93
TakshakRamteke <takshakramteke0708@gmail.com>
2022-10-23 20:28:18 +0530

    FIX : formating for Tg's Layout

--- components/Layouts/TgsLayout.tsx
+++ components/Layouts/TgsLayout.tsx
@@ -3,16 +3,16 @@ import TgSidebar from "components/Sidebar/TGsidebar";
 
 const TgsLayout = ({ children }: any) => {
   return (
-    <div className="flex flex-col lg:flex-row">
-      <section className="fixed h-screen overflow-hidden hidden lg:block">
+    <div className='flex flex-col lg:flex-row'>
+      <section className='fixed h-screen overflow-hidden hidden lg:block'>
         <TgSidebar />
       </section>
-      <section className="lg:hidden bg-white w-full h-12 p-2 z-30">
-        <div className="mr-1">
+      <section className='lg:hidden bg-white w-full h-12 p-2 z-30'>
+        <div className='mr-1'>
           <Header />
         </div>
       </section>
-      <section className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden p-8 lg:p-10 h-screen bg-slate-100 lg:ml-16 lg:items-center">
+      <section className='flex flex-col flex-1 overflow-y-auto overflow-x-hidden p-8 lg:p-10 h-screen bg-slate-100 lg:ml-16 lg:items-center'>
         <main>{children}</main>
       </section>
     </div>
