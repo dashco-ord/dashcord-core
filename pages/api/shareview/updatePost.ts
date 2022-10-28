09702ad99f9fef894f4cbd7136324c2d199fdb24
Takshak Ramteke <takshakramteke0708@gail.com>
2022-10-28 19:38:29 +0530

    FIX : author edits shareview

--- pages/api/shareview/updatePost.ts
+++ pages/api/shareview/updatePost.ts
@@ -1,39 +1,33 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { prisma } from "lib/prisma";
-import { getSession } from "next-auth/react";
-import { UserRole } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from 'lib/prisma';
+import { getSession } from 'next-auth/react';
+import { UserRole } from '@prisma/client';
 
 export default async function updatePost(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const session = await getSession({ req });
-  if (session?.role === UserRole.TNP) {
-    const { id, title, body, role, company, salary, criteria, link, tags } =
-      await req.body;
+  const { id, title, body, role, company, salary, criteria, link, tags } =
+    await req.body;
 
-    try {
-      await prisma.experience.update({
-        where: { id: id },
-        data: {
-          body: body,
-          title: title,
-          role: role,
-          company: company,
-          salary: salary,
-          criteria: criteria,
-          link: link,
-          tags: tags,
-        },
-      });
-      res.status(200).end();
-      return;
-    } catch (error) {
-      console.log(error);
-      res.status(500).end();
-    }
+  try {
+    await prisma.experience.update({
+      where: { id: id },
+      data: {
+        body: body,
+        title: title,
+        role: role,
+        company: company,
+        salary: salary,
+        criteria: criteria,
+        link: link,
+        tags: tags,
+      },
+    });
+    res.status(200).end();
+    return;
+  } catch (error) {
+    console.log(error);
+    res.status(500).end();
   }
-
-  res.status(405).end();
-  return;
 }
